3807ee186bf58ff14323dcfce6a89b3dd42c1f59
phamtranminhtri <tri.phamtranminh@hcmut.edu.vn>
2026-03-05 15:41:30 +0700

    Initial commit

---
+++ src/main/java/org/example/hanviet/HanVietApplication.java
@@ -0,0 +1,13 @@
+package org.example.hanviet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanVietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanVietApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hanviet/HanVietApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hanviet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanVietApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
