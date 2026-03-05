53d96ba6541223a815e50787698bb8eedcbcd8b3
phamtranminhtri <tri.phamtranminh@hcmut.edu.vn>
2026-03-05 17:08:54 +0700

    Upload image

--- src/main/java/org/example/hanviet/AppController.java
+++ src/main/java/org/example/hanviet/AppController.java
@@ -1,9 +1,44 @@
 package org.example.hanviet;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class AppController {
+
+    private final OcrService ocrService;
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads";
+
+    @Autowired
+    public AppController(OcrService ocrService) {
+        this.ocrService = ocrService;
+    }
+
+    @GetMapping("/uploadimage")
+    public String displayUploadForm() {
+        return "index";
+    }
+
+    @PostMapping("/upload")
+    public String uploadImage(Model model, @RequestParam("image") MultipartFile file) throws IOException {
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
+        fileNames.append(file.getOriginalFilename());
+        Files.write(fileNameAndPath, file.getBytes());
+        model.addAttribute("msg", "Uploaded images: " + fileNames.toString());
+        return "index";
+    }
+
 
 
 
