47fcd3b1986524e87c3fdad76ef0a36ef670fda0
phamtranminhtri <tri.phamtranminh@hcmut.edu.vn>
2026-03-07 06:45:20 +0700

    simple error handle

--- src/main/java/org/example/hanviet/AppController.java
+++ src/main/java/org/example/hanviet/AppController.java
@@ -18,6 +18,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -55,59 +57,69 @@ public class AppController {
     public RedirectView uploadImage(Model model, @RequestParam("image") MultipartFile file)
             throws IOException, ParseException, URISyntaxException, InterruptedException
     {
+        try {
 //        store file
-        ImageEntity imageEntity = new ImageEntity();
-        imageRepository.save(imageEntity);
-        imageEntity.setFilename(file.getOriginalFilename());
+            ImageEntity imageEntity = new ImageEntity();
+            imageRepository.save(imageEntity);
+            imageEntity.setFilename(file.getOriginalFilename());
 
-        String[] imageParts = file.getOriginalFilename().split("[.]");
-        String imageExtension = imageParts[imageParts.length - 1];
+            String[] imageParts = file.getOriginalFilename().split("[.]");
+            String imageExtension = imageParts[imageParts.length - 1];
 
-        Path tempFilePath = Paths.get(
-                UPLOAD_DIRECTORY, "%d.temp.%s".formatted(imageEntity.getId(), imageExtension)
-        );
-        Files.write(tempFilePath, file.getBytes());
+            Path tempFilePath = Paths.get(
+                    UPLOAD_DIRECTORY, "%d.temp.%s".formatted(imageEntity.getId(), imageExtension)
+            );
+            Files.write(tempFilePath, file.getBytes());
 
 //        Path compressedFilePath = Paths.get(
 //                "src/main/resources/static/image", "%d.jpg".formatted(imageEntity.getId())
 //        );
 
-        Path compressedFilePath = Paths.get(
-                UPLOAD_DIRECTORY, "%d.jpg".formatted(imageEntity.getId())
-        );
-        ocrService.compressImage(tempFilePath.toString(), compressedFilePath.toString());
+            Path compressedFilePath = Paths.get(
+                    UPLOAD_DIRECTORY, "%d.jpg".formatted(imageEntity.getId())
+            );
+            ocrService.compressImage(tempFilePath.toString(), compressedFilePath.toString());
 
 //        do OCR
-        String ocrJson = ocrService.doOcrImage(compressedFilePath.toString());
-        String rawHanzi = ocrService.parseOcr(ocrJson);
-        String filteredHanzi = ocrService.filterHanzi(rawHanzi);
-        imageEntity.setHanzi(filteredHanzi);
+            String ocrJson = ocrService.doOcrImage(compressedFilePath.toString());
+            String rawHanzi = ocrService.parseOcr(ocrJson);
+            String filteredHanzi = ocrService.filterHanzi(rawHanzi);
+            imageEntity.setHanzi(filteredHanzi);
 
 //        get Han Viet
-        List<List<String>> hanviets = ocrService.getHanviets(filteredHanzi);
+            List<List<String>> hanviets = ocrService.getHanviets(filteredHanzi);
 
-        StringBuilder stringBuilder = new StringBuilder();
-        for (List<String> stringList : hanviets) {
-            if (stringList.size() == 1) {
-                stringBuilder.append(stringList.getFirst());
-            } else {
-                stringBuilder.append("(");
-                stringBuilder.append(String.join("/", stringList));
-                stringBuilder.append(")");
+            StringBuilder stringBuilder = new StringBuilder();
+            for (List<String> stringList : hanviets) {
+                if (stringList.size() == 1) {
+                    stringBuilder.append(stringList.getFirst());
+                } else {
+                    stringBuilder.append("(");
+                    stringBuilder.append(String.join("/", stringList));
+                    stringBuilder.append(")");
+                }
+                stringBuilder.append(" ");
             }
-            stringBuilder.append(" ");
-        }
-        imageEntity.setHanviet(stringBuilder.toString());
+            imageEntity.setHanviet(stringBuilder.toString());
 
 //        get translation
-        String translationJson = ocrService.translate(filteredHanzi);
-        String translation = ocrService.parseTranslation(translationJson);
-        imageEntity.setTranslation(translation);
+            String translationJson = ocrService.translate(filteredHanzi);
+            String translation = ocrService.parseTranslation(translationJson);
+            imageEntity.setTranslation(translation);
 
-        imageRepository.save(imageEntity);
+            imageRepository.save(imageEntity);
 
-        model.addAttribute("msg", "Uploaded images: " + file.getOriginalFilename());
-        return new RedirectView("/view-image/%d".formatted(imageEntity.getId()));
+            model.addAttribute("msg", "Uploaded images: " + file.getOriginalFilename());
+            return new RedirectView("/view-image/%d".formatted(imageEntity.getId()));
+        } catch (Exception e) {
+            return new RedirectView("/app-error?q=%s".formatted(URLEncoder.encode(e.getMessage(), StandardCharsets.UTF_8)));
+        }
+    }
+
+    @GetMapping("/app-error")
+    public String error(Model model, @RequestParam String q) {
+        model.addAttribute("error", q);
+        return "app_error";
     }
 
     @GetMapping("/view-image/{id}")
